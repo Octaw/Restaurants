f0a3829a16bcdaca8711c63548175eff13f12e26
Octaw <j3zing@gmail.com>
2022-10-28 17:30:32 +0200

    Add Dto models and create validation

---
+++ Restaurants/Controllers/RestaurantController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Restaurants.Entities;
+using Restaurants.Models;
+using Restaurants.Services;
+
+namespace Restaurants.Controllers
+{
+    [Route("api/restaurant")]
+    public class RestaurantController : ControllerBase
+    {
+        private readonly IRestaurantService _restaurantService;
+        public RestaurantController(IRestaurantService restaurantService)
+        {
+            _restaurantService = restaurantService;
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete([FromRoute] int id)
+        {
+            var isDeleted = _restaurantService.Delete(id);
+
+            if(isDeleted)
+            {
+                return NoContent();
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public ActionResult CreateRestaurant([FromBody] CreateRestaurantDto dto)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var id = _restaurantService.Create(dto);
+
+            return Created($"/api/restaurant/{id}", null);
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<RestaurantDto>> GetAll()
+        {
+            var restaurantsDtos = _restaurantService.GetAll();
+            return Ok(restaurantsDtos);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<RestaurantDto> Get([FromRoute] int id)
+        {
+            var restaurant = _restaurantService.GetById(id);
+            if(restaurant is null)
+            {
+                return NotFound();
+            }
+            return Ok(restaurant);
+        }
+    }
+}
---
+++ Restaurants/RestaurantSeeder.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Restaurants.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurants
+{
+    public class RestaurantSeeder
+    {
+        private readonly RestaurantDbContext _dbContext;
+        public RestaurantSeeder(RestaurantDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public void Seed()
+        {
+            if(_dbContext.Database.CanConnect())
+            {
+                if(!_dbContext.Restaurants.Any())
+                {
+                    var restaurants = GetRestaurants();
+                    _dbContext.Restaurants.AddRange(restaurants);
+                    _dbContext.SaveChanges();
+                }
+            }
+        }
+        private IEnumerable<Restaurant> GetRestaurants()
+        {
+            var Restaurants = new List<Restaurant>()
+            {
+                new Restaurant()
+                {
+                    Name = "KFC",
+                    Category = "Fast Food",
+                    Description = "KFC (short for Kentucky Fried Chicken) is an American fast food restaurant",
+                    ContactEmail = "contact@kfc.com",
+                    HasDelivery = true,
+                    ContactNumber = "696969699",
+                    Dishes = new List<Dish>()
+                    {
+                        new Dish()
+                        {
+                            Name = "Nashville Hot Chicken",
+                            Price = 10.30M,
+                            Description = "Spicy"
+                        },
+                        new Dish()
+                        {
+                            Name = "Chicken Nuggets",
+                            Price = 5.30M,
+                            Description = "Ultra BIG"
+                        },
+                    },
+                    Address = new Address()
+                    {
+                        City = "Kraków",
+                        Street = "Długa 5",
+                        PostalCode = "30-001"
+                    }
+                },
+                new Restaurant()
+                {
+                    Name = "McDonalds",
+                    Category = "Fast Food",
+                    Description = "McDonald’s Corporation – amerykańska sieć barów szybkiej obsługi, sprzedająca głównie burgery, frytki i napoje.",
+                    ContactEmail = "contact@mc.com",
+                    HasDelivery = true,
+                    ContactNumber = "292934949",
+                    Dishes = new List<Dish>()
+                    {
+                        new Dish()
+                        {
+                            Name = "McRoyal",
+                            Price = 15.30M,
+                            Description = "With cheese"
+                        },
+                        new Dish()
+                        {
+                            Name = "Mc Nuggets",
+                            Price = 7.25M,
+                            Description = "small"
+                        },
+                    },
+                    Address = new Address()
+                    {
+                        City = "Rzeszów",
+                        Street = "Lwowska",
+                        PostalCode = "35-301"
+                    }
+                }
+            };
+            return Restaurants;
+        }
+    }
+}
